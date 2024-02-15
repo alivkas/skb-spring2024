85861a3d659b06e14307a041b7d59422edf9d340
alivkas <mr1504dj@mail.ru>
2024-02-15 14:53:13 +0500

    Initial commit

---
+++ src/main/java/com/example/skbspring2024/SkbSpring2024Application.java
@@ -0,0 +1,13 @@
+package com.example.skbspring2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkbSpring2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkbSpring2024Application.class, args);
+    }
+
+}
